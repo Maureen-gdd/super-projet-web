07c6721d0b48f36431dd3f7e47e9bfaa6879c36f
Maureen-gdd <maureen.grandidier@gmail.com>
2024-01-22 16:41:42 +0100

    ✨ [Component] Add Tamagotchi Card

---
+++ src/components/Tamagotchi.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="tamagotchi-card">
+      <img class="picture" v-bind:src="pictureUrl"/>
+      <div>
+        <h2 class="name">{{name}}</h2>
+        <p class="species">{{species}}</p>
+      </div>  
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'TamagotchiCard',
+      props: {
+        name: {type: String, required: true},
+        species: String, 
+        pictureUrl: {type: String, default:"https://images.radio-canada.ca/q_auto,w_1250/v1/ici-info/16x9/rick-astley-videoclip-never-gonna-give-you-up.png"}
+    }
+  }
+</script>
